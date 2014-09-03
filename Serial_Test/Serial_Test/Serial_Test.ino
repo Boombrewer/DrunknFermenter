045323357574bd05e538b09fcbc07b619579ce97
Casey <nomadic.jester@gmail.com>
2014-09-02 21:41:50 -0700

    Serial update
    Serial test updated to actually work and pass messages both ways.

--- Serial_Test/Serial_Test/Serial_Test.ino
+++ Serial_Test/Serial_Test/Serial_Test.ino
@@ -1,6 +1,7 @@
 // Serial test script
 
-int setPoint = 55;
+int setTemp1 = 55;
+int setTemp2 = 50;
 String readString;
 
 void setup()
@@ -12,10 +13,16 @@ void setup()
 
 void loop()
 {
-  while(!Serial.available()) {}
+  while(!Serial.available()) {
+  }
   // serial read section
   while (Serial.available())
   {
+    Serial.print("Set Temp 1: ");
+    Serial.println(setTemp1);
+    Serial.print("Set Temp 2: ");
+    Serial.println(setTemp2);
+    
     if (Serial.available() >0)
     {
       char c = Serial.read();  //gets one byte from serial buffer
@@ -23,20 +30,59 @@ void loop()
     }
   }
 
-  if (readString.length() >0)
+  if (readString.length() >=3)
   {
-    Serial.print("Arduino received: ");  
+    if (readString[0] == 'S' && readString[1] == '1')
+    {
+      String set1 = readString.substring(2,4);
+      setTemp1 = set1.toInt();
+      //String setString;
+      //setString = String(setTemp1);
+
+      //Serial.print("Set Temp 1: ");
+      //Serial.println(setString);
+      //Serial.print("\n");
+    }
+  }
+
+  String setString1;
+  setString1 = String(setTemp1);
+  Serial.print("New Set Temp 1: ");
+  Serial.println(setString1);
+  Serial.print("\n");
+
+  if (readString.length() >=8)
+  {
+    if (readString[4] == 'S' && readString[5] == '2')
+    {
+      String set2 = readString.substring(6,8);
+      setTemp2 = set2.toInt();
+    }
+  }
+
+  String setString2;
+  setString2 = String(setTemp2);
+  Serial.print("New Set Temp 2: ");
+  Serial.println(setString2);
+  Serial.print("\n");
+
+  if (readString.length() >3)
+  {
+    Serial.println("Arduino received: ");  
     Serial.println(readString); //see what was received
+    readString = "";
   }
 
   delay(500);
 
   // serial write section
 
-  char ard_sends = '1';
-  Serial.print("Arduino sends: ");
-  Serial.println(ard_sends);
-  Serial.print("\n");
+
   Serial.flush();
 }
+
+
+
+
+
 
